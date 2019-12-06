9bdca9f6577193782043221e99518f58cd73b853
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-12-06 16:47:24 +0100

    Add nbsphinx ext and md file format to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,8 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
 	'sphinx.ext.napoleon',
-	'sphinx.ext.todo'
+	'sphinx.ext.todo',
+    'nbsphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,7 +46,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
