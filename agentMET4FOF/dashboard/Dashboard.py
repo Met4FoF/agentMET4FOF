3f0167d2c67b982e5703bbc4eea957ae216c937f
BANG XIANG YONG <yongbangxiang@gmail.com>
2019-12-08 22:07:15 +0000

    fixed bug for first time refresh - weird behaviour when dcc table or graph is placed in 2nd page of tabs, the callbacks are not fired for first time. - solution: removed content from tabs component, and placing it outside of the tabs

--- agentMET4FOF/dashboard/Dashboard.py
+++ agentMET4FOF/dashboard/Dashboard.py
@@ -1,21 +1,8 @@
 # -*- coding: utf-8 -*-
-import os
 import dash
-import dash_cytoscape as cyto
 import dash_html_components as html
 import dash_core_components as dcc
 
-from dash.exceptions import PreventUpdate
-
-import plotly.graph_objs as go
-from plotly import tools
-from plotly.subplots import make_subplots
-
-import networkx as nx
-import agentMET4FOF.agents as agentmet4fof_module
-
-import agentMET4FOF.dashboard.LayoutHelper as LayoutHelper
-from agentMET4FOF.dashboard.LayoutHelper import create_nodes_cytoscape, create_edges_cytoscape, create_monitor_graph
 from agentMET4FOF.dashboard.Dashboard_ml_exp import get_ml_exp_layout, prepare_ml_exp_callbacks, get_experiments_list
 from agentMET4FOF.dashboard.Dashboard_agt_net import get_agt_net_layout, prepare_agt_net_callbacks
 
@@ -26,7 +13,6 @@ app = dash.Dash(__name__,
                 external_stylesheets=external_stylesheets,
                 external_scripts=external_scripts
                 )
-
 
 def init_app_layout(app,update_interval_seconds=3,num_monitors=10):
     app.update_interval_seconds = update_interval_seconds
@@ -42,34 +28,25 @@ def init_app_layout(app,update_interval_seconds=3,num_monitors=10):
             ], className="light-blue lighten-1"),
             dcc.Tabs(id="main-tabs", value="agt-net", children=[
                 dcc.Tab(id="agt-net-tab", value="agt-net",label='Agent Network', children=[
-                    get_agt_net_layout(update_interval_seconds,num_monitors)
                 ]),
                 dcc.Tab(id="ml-exp-tab",value="ml-exp", label='ML Experiments',  children=[
-                    get_ml_exp_layout()
                 ]),
             ]),
-            # html.Div(get_agt_net_layout(update_interval_seconds,num_monitors))
-            dcc.Interval(
-                id='interval-first-load',
-                interval=1000 * 1000,  # in milliseconds
-                n_intervals=0
-            )
+            html.Div(id="page-div",children=[
+            get_agt_net_layout(update_interval_seconds,app.num_monitors),
+            get_ml_exp_layout(),
+            ]),
     ])
     prepare_agt_net_callbacks(app)
     prepare_ml_exp_callbacks(app)
 
-
-    @app.callback([dash.dependencies.Output('agt-net-tab', 'children'),
-                  dash.dependencies.Output('ml-exp-tab', 'children')],
-                  [dash.dependencies.Input('main-tabs', 'value'),
-                   dash.dependencies.Input('interval-first-load', 'n_intervals')
-                   ])
-    def render_content(tab,intervals):
+    @app.callback([dash.dependencies.Output('page-div', 'children')],
+                  [dash.dependencies.Input('main-tabs', 'value')])
+    def render_content(tab):
         if tab == 'ml-exp':
             experiments_df = get_experiments_list()
-            return ["",get_ml_exp_layout(experiments_df)]
+            return [get_ml_exp_layout(experiments_df)]
         else:
-
-            return [get_agt_net_layout(app.update_interval_seconds,app.num_monitors),""]
+            return [get_agt_net_layout(app.update_interval_seconds,app.num_monitors)]
 
     return app
