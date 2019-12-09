1728b82fad84e00295d2aee2a19e72e33901b16f
BANG XIANG YONG <yongbangxiang@gmail.com>
2019-12-09 09:38:40 +0000

    cleaned examples for ML Experiment - more work can be done to illustrate and give more details

--- examples/ML_EXPERIMENTS/ml_experiment_complex.py
+++ examples/ML_EXPERIMENTS/ml_experiment_complex.py
@@ -1,6 +1,10 @@
-# import os
-# path = "F:/PhD Research/Github/develop_ml_experiments_met4fof/agentMET4FOF"
-# os.chdir(path)
+"""
+A more complicated pipeline is experimented here, with a larger range of hyperparameters.
+Since this implies a grid search, the computation can be too intensive if the pipeline and
+hyperparameter range are made too large.
+"""
+
+
 
 from agentMET4FOF.agents import AgentMET4FOF, AgentNetwork, MonitorAgent, AgentPipeline
 from agentMET4FOF.develop.datastream import *
@@ -26,34 +30,30 @@ from agentMET4FOF.ml_uncertainty.evaluate_pred_unc import *
 if __name__ == "__main__":
     agentNetwork = AgentNetwork()
 
-    ml_exp_name = "mm_pipeline"
+    ml_exp_name = "complex"
 
-    ML_Agent_pipelines_B = AgentPipeline(agentNetwork,
-                                             [StandardScaler],
-                                             [BNN_Model], hyperparameters=[[],
-                                                                           [{"num_epochs":[500,1000,1500],"task":["classification"],"architecture":[["d1","d1"],["d1","d1","d1"],["d1","d1","d1","d1"]]}]
-                                                                           ])
-
-
-
+    ML_Agent_pipelines_A = AgentPipeline(agentNetwork,
+                                         [StandardScaler, RobustScaler,MinMaxScaler,MaxAbsScaler,PowerTransformer],
+                                         [PCA],
+                                         [BNN_Model], hyperparameters=[[],[],[{"num_epochs":[500,1000,1500],
+                                                                               "task":["classification"],
+                                                                               "architecture":[["d1","d1"],["d1","d1","d1"]]}]])
 
 
     #init
     datastream_agent = agentNetwork.add_agent(agentType=DataStreamAgent)
     evaluation_agent = agentNetwork.add_agent(agentType=EvaluationAgent)
-    monitor_agent = agentNetwork.add_agent(agentType=MonitorAgent)
 
     datastream_agent.init_parameters(data_name="iris", x=datasets.load_iris().data,y=datasets.load_iris().target)
     evaluation_agent.init_parameters([f1_score,p_acc_unc,avg_unc],[{"average":'micro'},{},{}], ML_exp=True)
 
     #setup ml experiment
-    ml_experiment = ML_Experiment(datasets=[datastream_agent], pipelines=[ML_Agent_pipelines_B], evaluation=[evaluation_agent], name=ml_exp_name, train_mode="Kfold5")
+    ml_experiment = ML_Experiment(datasets=[datastream_agent], pipelines=[ML_Agent_pipelines_A], evaluation=[evaluation_agent], name=ml_exp_name, train_mode="Kfold5")
     agentNetwork.get_agent("Logger").set_ml_experiment(ml_experiment)
-    print(ml_experiment.pipeline_details)
-    print(ml_experiment.chain_results)
+
+    #optional: connect evaluation agent to monitor agent
+    monitor_agent = agentNetwork.add_agent(agentType=MonitorAgent)
+    evaluation_agent.bind_output(monitor_agent)
 
     #set to active running
     agentNetwork.set_running_state()
-
-    #save and load file
-    save_experiment(agentNetwork.get_agent("Logger").get_attr("ml_experiment"))
