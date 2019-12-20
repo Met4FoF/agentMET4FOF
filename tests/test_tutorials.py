47aa9b0b28a4821c07f2a6574c0286e483c183cc
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-12-20 17:58:48 +0100

    Refactor and improve tutorial_1...py

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -1,10 +1,11 @@
-from tutorials.tutorial_1_generator_agent import main as tutorial_1_main
+from tutorials.tutorial_1_generator_agent import demonstrate_generator_agent_use as tut1
 from tutorials.tutorial_2_math_agent import main as tutorial_2_main
 from tutorials.tutorial_3_multi_channel import main as tutorial_3_main
 
 
 def test_tutorial_1():
-    tutorial_1_main().shutdown()
+    # Test executability of tutorial_1_generator_agent.
+    tut1().shutdown()
 
 
 def test_tutorial_2():
