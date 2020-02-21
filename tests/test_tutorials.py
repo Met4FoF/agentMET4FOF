b7205710e36dfa54392fb9ae502f049f0a507e63
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-02-21 19:56:09 +0100

    Fix tests for tutorials

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -1,6 +1,6 @@
-from tutorials.tutorial_1_generator_agent import main as tutorial_1_main
-from tutorials.tutorial_2_math_agent import main as tutorial_2_main
-from tutorials.tutorial_3_multi_channel import main as tutorial_3_main
+from agentMET4FOF_tutorials.tutorial_1_generator_agent import main as tutorial_1_main
+from agentMET4FOF_tutorials.tutorial_2_math_agent import main as tutorial_2_main
+from agentMET4FOF_tutorials.tutorial_3_multi_channel import main as tutorial_3_main
 
 
 def test_tutorial_1():
