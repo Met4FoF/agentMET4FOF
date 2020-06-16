e9a8a098b9efb1e25baba3efaa6a0a5a5c3772fb
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-06-16 19:54:10 +0200

    Realign imports in ReadTheDocs' conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,11 +17,12 @@ import os
 import shutil
 import sys
 
+from recommonmark.parser import CommonMarkParser
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
-from recommonmark.parser import CommonMarkParser
 
 sys.path.insert(0, os.path.abspath(".."))
 
