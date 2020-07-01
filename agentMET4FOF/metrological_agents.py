cf0c17dabeabd9ea4b1fa7f6925069e7362da24f
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-07-01 13:54:39 +0200

    Introduce mgrub's reference implementation for a metrologically enabled agent

---
+++ agentMET4FOF/metrological_agents.py
@@ -0,0 +1,57 @@
+from agentMET4FOF.agents import AgentMET4FOF as Agent
+from time_series_buffer import TimeSeriesBuffer
+
+
+class MetroAgent(Agent):
+    def init_parameters(self):
+
+        # TODO: for every connected agent, init the below objects
+        # dict of {<from> : {"buffer" : TimeSeriesBuffer(maxlen=buffer_size), "metadata" : MetaData(**kwargs).metadata}}
+        self.input_data = {}
+
+        # dict of {<channel> : {"buffer" : TimeSeriesBuffer(maxlen=buffer_size), "metadata" : MetaData(**kwargs)}
+        self.output_data = {}
+
+    def on_received_message(self, message):
+        channel = message["channel"]
+        sender = message["from"]
+
+        if channel == "default":
+            data = message["data"]
+            self.append_to_input_buffer(sender, data)
+
+        elif channel == "metadata":
+            metadata = message["data"]
+            self.set_metadata(sender, metadata)
+
+    def append_to_input_buffer(self, sender, data):
+        self.input_data[sender]["buffer"].append(data=data)
+
+    def set_metadata(self, sender, metadata):
+        self.input_data[sender]["metadata"] = metadata
+
+    def agent_loop(self):
+        if self.current_state == "Running":
+
+            for channel, channel_dict in self.output_data.items():
+
+                # send metadata
+                metadata = channel_dict["metadata"]
+                self.send_output({self.name: metadata}, channel="metadata")
+
+                # send data
+                buffer = channel_dict["buffer"]
+                buffer_len = len(buffer)
+                if buffer_len:
+                    data = buffer.pop(n_samples=buffer_len)
+                    self.send_output({self.name: data}, channel=channel)
+
+    # def bind_output(self, output_agent):
+    #     parent().bind_output()
+
+
+class MetroPlotAgent(MetroAgent):
+    pass
+
+    def custom_plot_function(self):
+        pass
