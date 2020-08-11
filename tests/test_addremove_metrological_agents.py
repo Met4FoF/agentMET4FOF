27051577c9090f8d2c8aa1b4ade2e9a3f965e571
Anupam Prasad Vedurmudi <anupam.13@gmail.com>
2020-08-11 14:34:15 +0200

    first simple test added for module metrological_agents

---
+++ tests/test_addremove_metrological_agents.py
@@ -0,0 +1,28 @@
+from agentMET4FOF.agents import AgentNetwork
+from agentMET4FOF.metrological_agents import MetrologicalAgent
+import time
+
+def test_addremove_metrological_agents():
+    agentNetwork = AgentNetwork(dashboard_modules=False)
+
+    #init agents by adding into the agent network
+    dummy_agent1 = agentNetwork.add_agent(agentType=MetrologicalAgent)
+    dummy_agent2 = agentNetwork.add_agent(agentType=MetrologicalAgent)
+    dummy_agent3 = agentNetwork.add_agent(agentType=MetrologicalAgent)
+    dummy_agent4 = agentNetwork.add_agent(agentType=MetrologicalAgent)
+
+    agentNetwork.bind_agents(dummy_agent1, dummy_agent2)
+    agentNetwork.bind_agents(dummy_agent1, dummy_agent3)
+    agentNetwork.bind_agents(dummy_agent1, dummy_agent4)
+
+    agentNetwork.bind_agents(dummy_agent4, dummy_agent2)
+    agentNetwork.bind_agents(dummy_agent3, dummy_agent4)
+    agentNetwork.bind_agents(dummy_agent2, dummy_agent4)
+
+    agentNetwork.remove_agent(dummy_agent1)
+    agentNetwork.remove_agent(dummy_agent2)
+    agentNetwork.remove_agent(dummy_agent3)
+    agentNetwork.remove_agent(dummy_agent4)
+    time.sleep(2)
+    assert len(agentNetwork.agents()) == 0
+    agentNetwork.shutdown()
