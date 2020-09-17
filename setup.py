aa7a989532ac976c4381fc3f5be8cf359a1780f9
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-09-17 09:18:04 +0200

    Pin matplotlib to something below 3.3.0 really in setup.py as well

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     install_requires=[
         "numpy",
         "scipy",
-        "matplotlib<=3.3.0",
+        "matplotlib<3.3.0",
         "pandas",
         "osbrain",
         "dash",
