6d1c8c7b04838d80f0106b03d66598232ee95a73
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2020-12-10 15:44:42 +0100

    Repair test which is supposed to fail on current version of dashboard teardown with backend "mesa"

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,8 +13,8 @@ np.random.seed(123)
 
 
 @pytest.fixture
-def agent_network(backend="osbrain"):
+def agent_network(request):
     # Create an agent network and shut it down after usage.
-    a_network = AgentNetwork(dashboard_modules=False, backend=backend)
+    a_network = AgentNetwork(dashboard_modules=False, backend=request.param)
     yield a_network
     a_network.shutdown()
