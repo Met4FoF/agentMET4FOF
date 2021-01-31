78883939db3aaad70ccb1568eca72d08dac8de70
BANG XIANG YONG <yongbangxiang@gmail.com>
2021-01-31 03:29:39 +0000

    minor update to dummy example

--- develop_UI/agent_main_example.py
+++ develop_UI/agent_main_example.py
@@ -1,9 +1,16 @@
-from agentMET4FOF.agents import AgentNetwork
+from agentMET4FOF.agents import AgentNetwork, MonitorAgent
+from agentMET4FOF_tutorials.tutorial_1_generator_agent import SineGeneratorAgent
 from develop_UI import agent_module_example
 
 def demonstrate_generator_agent_use():
     # Start agent network server.
-    agent_network = AgentNetwork(dashboard_modules=[agent_module_example])
+    # agent_network = AgentNetwork(dashboard_modules=[agent_module_example])
+    agent_network = AgentNetwork(dashboard_modules=[agent_module_example], backend="mesa")
+    sine_agent = agent_network.add_agent(agentType=SineGeneratorAgent)
+
+    coalition = agent_network.add_coalition(agents=[])
+
+    agent_network.add_coalition_agent(name=coalition.name,agents=[sine_agent])
 
     return agent_network
 
