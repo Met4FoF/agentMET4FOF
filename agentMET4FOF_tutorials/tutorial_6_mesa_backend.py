cc219f38de4bd254fa7a6617ec00e813d04a33a5
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-02-02 15:52:18 +0100

    Introduce Mesa example

--- agentMET4FOF_tutorials/tutorial_6_mesa_backend.py
+++ agentMET4FOF_tutorials/tutorial_6_mesa_backend.py
@@ -1,18 +1,14 @@
 from agentMET4FOF.agents import AgentNetwork, MonitorAgent, SineGeneratorAgent
 
 
-def demonstrate_metrological_stream():
+def demonstrate_mesa_backend():
 
-    # start agent network server
-    _agent_network = AgentNetwork(dashboard_modules=True, backend="mesa")
+    # Start agent network and specify backend via the corresponding keyword parameter.
+    _agent_network = AgentNetwork(backend="mesa")
 
-    # Initialize metrologically enabled agent taking name from signal source metadata.
+    # Initialize agents by adding them to the agent network.
     sine_agent = _agent_network.add_agent(agentType=SineGeneratorAgent)
-
-    # Initialize metrologically enabled plotting agent.
     monitor_agent = _agent_network.add_agent(agentType=MonitorAgent, buffer_size=200)
-
-    # Bind agents.
     sine_agent.bind_output(monitor_agent)
 
     # Set all agents states to "Running".
@@ -22,4 +18,4 @@ def demonstrate_metrological_stream():
 
 
 if __name__ == "__main__":
-    demonstrate_metrological_stream()
+    demonstrate_mesa_backend()
