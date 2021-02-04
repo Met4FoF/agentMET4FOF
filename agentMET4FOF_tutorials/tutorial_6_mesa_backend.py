4188ad91b0c483ea9862679e8cef337bec36e8db
BANG XIANG YONG <yongbangxiang@gmail.com>
2021-02-04 12:35:04 +0000

    added explanations on tutorial 6

--- agentMET4FOF_tutorials/tutorial_6_mesa_backend.py
+++ agentMET4FOF_tutorials/tutorial_6_mesa_backend.py
@@ -1,3 +1,10 @@
+# By default, "osbrain" backend offers real connectivity between agents (each agent has its own port & IP address) in distributed systems (e,g connecting agents from raspberry pis to PCs, etc),
+# which explains why it is harder to debug.
+# In the "mesa" backend, there's only one real timer which is started in the AgentNetwork, and every timer tick will advance the agent actions by calling `step()` which includes `agent_loop` and `on_received_message`.
+# Moreover, in the "mesa" backend, agents do not have their own port and IP addresses, they are simulated objects to emulate the behaviour of distributed agents.
+# Hence, "osbrain" is closer to deployment phase, whereas mesa is suited for the simulation/designing phase.
+# To switch between the backends, simply pass the backend parameter to either "mesa" or "osbrain" in the AgentNetwork instantiation.
+
 from agentMET4FOF.agents import AgentNetwork, MonitorAgent, SineGeneratorAgent
 
 
