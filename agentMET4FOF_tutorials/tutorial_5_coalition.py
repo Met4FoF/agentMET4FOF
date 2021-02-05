cdb8b95b7f2c7658f284ffcd1dc41d802521e683
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-02-05 20:42:15 +0100

    Introduce coalitions tutorial's notebook

--- agentMET4FOF_tutorials/tutorial_5_coalition.py
+++ agentMET4FOF_tutorials/tutorial_5_coalition.py
@@ -1,42 +1,4 @@
-from agentMET4FOF.agents import AgentMET4FOF, AgentNetwork, MonitorAgent
-from agentMET4FOF.streams import SineGenerator
-
-# We demonstrate the use of Coalition of agents to group agents together
-# Rationale of grouping depends on the users and application
-# For example, we can group sensors which are measuring the same measurand
-# To this end, the coalition consists of a list of agent names and
-# provides aesthetic differences in the dashboard
-# All coalitions visible by the `agent_network` can be accessed via
-# `agent_network.coalitions`.
-
-
-class SineGeneratorAgent(AgentMET4FOF):
-    """An agent streaming a sine signal
-
-    Takes samples from the :py:mod:`SineGenerator` and pushes them sample by sample
-    to connected agents via its output channel.
-    """
-
-    # The datatype of the stream will be SineGenerator.
-    _sine_stream: SineGenerator
-
-    def init_parameters(self):
-        """Initialize the input data
-
-        Initialize the input data stream as an instance of the
-        :py:mod:`SineGenerator` class
-        """
-        self._sine_stream = SineGenerator()
-
-    def agent_loop(self):
-        """Model the agent's behaviour
-
-        On state *Running* the agent will extract sample by sample the input data
-        streams content and push it via invoking :py:method:`AgentMET4FOF.send_output`.
-        """
-        if self.current_state == "Running":
-            sine_data = self._sine_stream.next_sample()  # dictionary
-            self.send_output(sine_data["x"])
+from agentMET4FOF.agents import AgentNetwork, MonitorAgent, SineGeneratorAgent
 
 
 def demonstrate_generator_agent_use():
