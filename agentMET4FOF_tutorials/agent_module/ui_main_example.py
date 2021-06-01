3a0342875c4ea46582b7dab49cd8a7b10aa83b37
BANG XIANG YONG <yongbangxiang@gmail.com>
2021-06-01 11:31:42 +0100

    docs(enhance_UI): rename and add description to UI example

--- agentMET4FOF_tutorials/agent_module/ui_main_example.py
+++ agentMET4FOF_tutorials/agent_module/ui_main_example.py
@@ -1,10 +1,16 @@
-from agentMET4FOF.agents import AgentNetwork, MonitorAgent
+# This main example shows how to create and import an agent defined in agent_module_example.py
+# Firstly, it is done via python import
+# Then, in the instantiation of the AgentNetwork , we pass the `agent_module_example`
+# All agents imported will be then available via the dashboard UI
+# Note also, the exposed and tunable parameters in the `ParameterisedSineGeneratorAgent` are defined in the class
+
+from agentMET4FOF.agents import AgentNetwork
 from agentMET4FOF_tutorials.tutorial_1_generator_agent import SineGeneratorAgent
-from develop_UI import agent_module_example
+from agentMET4FOF_tutorials.agent_module import agent_module_example
+
 
 def demonstrate_generator_agent_use():
     # Start agent network server.
-    # agent_network = AgentNetwork(dashboard_modules=[agent_module_example])
     agent_network = AgentNetwork(dashboard_modules=[agent_module_example], backend="mesa")
     sine_agent = agent_network.add_agent(agentType=SineGeneratorAgent)
 
