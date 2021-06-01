58b990b39e6f32bca5f1730bf24ac4e6f3216550
BANG XIANG YONG <yongbangxiang@gmail.com>
2021-06-01 11:32:30 +0100

    fix(enhance_UI): fix importing name for UI example

--- agentMET4FOF_tutorials/agent_module/ui_main_example.py
+++ agentMET4FOF_tutorials/agent_module/ui_main_example.py
@@ -1,4 +1,4 @@
-# This main example shows how to create and import an agent defined in agent_module_example.py
+# This main example shows how to create and import an agent defined in ui_module_example.py
 # Firstly, it is done via python import
 # Then, in the instantiation of the AgentNetwork , we pass the `agent_module_example`
 # All agents imported will be then available via the dashboard UI
@@ -6,12 +6,12 @@
 
 from agentMET4FOF.agents import AgentNetwork
 from agentMET4FOF_tutorials.tutorial_1_generator_agent import SineGeneratorAgent
-from agentMET4FOF_tutorials.agent_module import agent_module_example
+from agentMET4FOF_tutorials.agent_module import ui_module_example
 
 
 def demonstrate_generator_agent_use():
     # Start agent network server.
-    agent_network = AgentNetwork(dashboard_modules=[agent_module_example], backend="mesa")
+    agent_network = AgentNetwork(dashboard_modules=[ui_module_example], backend="mesa")
     sine_agent = agent_network.add_agent(agentType=SineGeneratorAgent)
 
     coalition = agent_network.add_coalition(agents=[])
