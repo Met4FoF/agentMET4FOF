e604418516ee6adb62bb881d9cbd211d441e57a2
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-06-29 18:59:36 +0200

    refactor(signal_streams): refactor signal_streams.py by applying black

--- agentMET4FOF/streams/signal_streams.py
+++ agentMET4FOF/streams/signal_streams.py
@@ -19,10 +19,20 @@ class SineGenerator(DataStreamMET4FOF):
     sine_freq : float
         frequency of wave function
     """
+
     def __init__(self, sfreq=500, sine_freq=50):
         super().__init__()
-        self.set_metadata("SineGenerator","time","s",("Voltage"),("V"),"Simple sine wave generator")
-        self.set_generator_function(generator_function=self.sine_wave_function, sfreq=sfreq, sine_freq=sine_freq)
+        self.set_metadata(
+            "SineGenerator",
+            "time",
+            "s",
+            ("Voltage"),
+            ("V"),
+            "Simple sine wave generator",
+        )
+        self.set_generator_function(
+            generator_function=self.sine_wave_function, sfreq=sfreq, sine_freq=sine_freq
+        )
 
     def sine_wave_function(self, time, sine_freq):
         """A simple sine wave generator"""
@@ -47,10 +57,22 @@ class CosineGenerator(DataStreamMET4FOF):
     F : int
         frequency of wave function
     """
-    def __init__(self, sfreq = 500, cosine_freq=5):
+
+    def __init__(self, sfreq=500, cosine_freq=5):
         super().__init__()
-        self.set_metadata("CosineGenerator","time","s",("Voltage"),("V"),"Simple cosine wave generator")
-        self.set_generator_function(generator_function=self.cosine_wave_function, sfreq=sfreq, cosine_freq=cosine_freq)
+        self.set_metadata(
+            "CosineGenerator",
+            "time",
+            "s",
+            ("Voltage"),
+            ("V"),
+            "Simple cosine wave generator",
+        )
+        self.set_generator_function(
+            generator_function=self.cosine_wave_function,
+            sfreq=sfreq,
+            cosine_freq=cosine_freq,
+        )
 
     def cosine_wave_function(self, time, cosine_freq=50):
         """A simple cosine wave generator"""
