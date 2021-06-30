48a80332c80a37bde78af6b9f97ac1973a8ef55e
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-06-30 09:52:19 +0200

    wip(agents): introduce module docstring

--- agentMET4FOF/agents/__init__.py
+++ agentMET4FOF/agents/__init__.py
@@ -1,2 +1,8 @@
+"""This module ensures backwards compatibility of the module to package refactoring
+
+Up until version 0.8.1 we had several modules instead of the packages `agents` and
+`streams`. We have included this package to not break the previously needed import
+statements.
+"""
 from .base_agents import AgentMET4FOF, AgentNetwork, MonitorAgent, AgentBuffer
 from .signal_agents import MetrologicalGeneratorAgent, SineGeneratorAgent
