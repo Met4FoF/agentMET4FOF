63bf3cb41b1104f70526d6530f50c093ac8345bc
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-06-30 09:52:38 +0200

    wip(streams): introduce module docstring

--- agentMET4FOF/streams/__init__.py
+++ agentMET4FOF/streams/__init__.py
@@ -1,2 +1,8 @@
+"""This module ensures backwards compatibility of the module to package refactoring
+
+Up until version 0.8.1 we had several modules instead of the packages `agents` and
+`streams`. We have included this package to not break the previously needed import
+statements.
+"""
 from .base_streams import DataStreamMET4FOF
 from .signal_streams import SineGenerator, CosineGenerator
