9da3744972737c8c1a863e4712c85385348b3d0e
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-06-30 08:16:57 +0200

    refactor(metrological_streams): refactor metrological_streams.py into a package metrological_streams and include classes into streams

---
+++ agentMET4FOF/metrological_streams/__init__.py
@@ -0,0 +1,17 @@
+"""This module ensures backwards compatibility of the module to package refactoring
+
+Up until version 0.8.1 we had several modules instead of the packages `agents` and
+`streams`. We have included this package to not break the previously needed import
+statements.
+"""
+from ..streams.base_streams import MetrologicalDataStreamMET4FOF
+from ..streams.signal_streams import (
+    MetrologicalMultiWaveGenerator,
+    MetrologicalSineGenerator,
+)
+
+__all__ = [
+    "MetrologicalDataStreamMET4FOF",
+    "MetrologicalMultiWaveGenerator",
+    "MetrologicalSineGenerator",
+]
