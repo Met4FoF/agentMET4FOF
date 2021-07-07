9678493d1ad5789f7d33d93f114fe418ea9e1f99
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-07-07 17:47:26 +0200

    wip(base_streams): remove some left-over code from the extraction of the examples

--- agentMET4FOF/streams/base_streams.py
+++ agentMET4FOF/streams/base_streams.py
@@ -380,26 +380,3 @@ class DataStreamMET4FOF:
 
     def has_more_samples(self):
         return self._sample_idx < self._n_samples
-
-
-# Built-in classes with DataStreamMET4FOF
-
-
-def extract_x_y(message):
-    """
-    Extracts features & target from ``message['data']`` with expected structure such as:
-
-    1. tuple - (x,y)
-    2. dict - {'x':x_data,'y':y_data}
-
-    Handle data structures of dictionary to extract features & target
-    """
-    if type(message["data"]) == tuple:
-        x = message["data"][0]
-        y = message["data"][1]
-    elif type(message["data"]) == dict:
-        x = message["data"]["x"]
-        y = message["data"]["y"]
-    else:
-        return 1
-    return x, y
