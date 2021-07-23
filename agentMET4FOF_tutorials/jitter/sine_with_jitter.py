86669b6e1d06c8d2558e45de05e5ca8c78881c37
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-07-23 19:10:48 +0200

    wip(sine_with_jitter): introduce first draft of an example comparing a clean and signal with jitter

---
+++ agentMET4FOF_tutorials/jitter/sine_with_jitter.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from agentMET4FOF.agents import (
+    AgentNetwork,
+    MonitorAgent,
+    SineGeneratorAgent,
+)
+from agentMET4FOF.agents.signal_agents import StaticSineGeneratorWithJitterAgent
+
+
+def demonstrate_generator_agent_use():
+    agent_network = AgentNetwork(backend="mesa")
+
+    sine_agent = agent_network.add_agent(agentType=SineGeneratorAgent)
+    sine_agent.init_parameters(sfreq=395, sine_freq=2 * np.pi)
+    jitter_agent = agent_network.add_agent(agentType=StaticSineGeneratorWithJitterAgent)
+    monitor_agent = agent_network.add_agent(agentType=MonitorAgent)
+
+    agent_network.bind_agents(sine_agent, monitor_agent)
+    agent_network.bind_agents(jitter_agent, monitor_agent)
+
+    agent_network.set_running_state()
+
+    return agent_network
+
+
+if __name__ == "__main__":
+    demonstrate_generator_agent_use()
