fddb429818e53e18be0dc56f6d5e74a6347468d4
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-07-23 19:19:49 +0200

    wip(sine_with_jitter): improve plot by naming agents

--- agentMET4FOF_tutorials/jitter/sine_with_jitter.py
+++ agentMET4FOF_tutorials/jitter/sine_with_jitter.py
@@ -11,9 +11,13 @@ from agentMET4FOF.agents.signal_agents import StaticSineGeneratorWithJitterAgent
 def demonstrate_generator_agent_use():
     agent_network = AgentNetwork(backend="mesa")
 
-    sine_agent = agent_network.add_agent(agentType=SineGeneratorAgent)
+    sine_agent = agent_network.add_agent(
+        name="Clean sine signal", agentType=SineGeneratorAgent
+    )
     sine_agent.init_parameters(sfreq=395, sine_freq=2 * np.pi)
-    jitter_agent = agent_network.add_agent(agentType=StaticSineGeneratorWithJitterAgent)
+    jitter_agent = agent_network.add_agent(
+        name="Sine signal with jitter", agentType=StaticSineGeneratorWithJitterAgent
+    )
     monitor_agent = agent_network.add_agent(agentType=MonitorAgent)
 
     agent_network.bind_agents(sine_agent, monitor_agent)
