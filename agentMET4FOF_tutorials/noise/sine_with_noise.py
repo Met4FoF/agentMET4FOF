2b7d75a50bfba6c825b6ab9fdf3fa9a4ae5cb1a7
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-07-27 19:12:42 +0200

    wip(sine_with_noise): introduce plot title into example

--- agentMET4FOF_tutorials/noise/sine_with_noise.py
+++ agentMET4FOF_tutorials/noise/sine_with_noise.py
@@ -14,7 +14,9 @@ def demonstrate_generator_agent_use():
     noise_agent = agent_network.add_agent(
         name="Sine signal with noise", agentType=NoiseAgent
     )
-    monitor_agent = agent_network.add_agent(agentType=MonitorAgent)
+    monitor_agent = agent_network.add_agent(
+        name="Compare clean and noisy signal", agentType=MonitorAgent
+    )
 
     agent_network.bind_agents(sine_agent, monitor_agent)
     agent_network.bind_agents(sine_agent, noise_agent)
