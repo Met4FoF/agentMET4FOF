cbcceb85e0790dabf886f28abcf24440b1d8d51c
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-07-29 12:04:06 +0200

    wip(test_tutorials): refactor by moving comments into function names

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -29,49 +29,39 @@ from agentMET4FOF_tutorials.tutorial_7_generic_metrological_agent import (
 )
 
 
-
-def test_tutorial_1():
-    """Test executability of tutorial_1_generator_agent."""
+def test_tutorial_1_executability():
     tut1().shutdown()
 
 
-def test_tutorial_2():
-    """Test executability of tutorial_2_math_agent."""
+def test_tutorial_2_executability():
     tut2().shutdown()
 
 
-def test_tutorial_3():
-    """Test executability of tutorial_3_multi_channel."""
+def test_tutorial_3_executability():
     tut3().shutdown()
 
 
-def test_tutorial_4():
-    """Test executability of tutorial_4_metrological_streams.py."""
+def test_tutorial_4_executability():
     tut4().shutdown()
 
 
-def test_tutorial_5():
-    """Test executability of tutorial_5_coalition.py."""
+def test_tutorial_5_executability():
     tut5().shutdown()
 
 
-def test_tutorial_6():
-    """Test executability of tutorial_6_mesa_backend.py."""
+def test_tutorial_6_executability():
     tut6().shutdown()
 
 
-def test_tutorial_7():
-    """Test executability of tutorial_7_generic_metrological_agent.py."""
+def test_tutorial_7_executability():
     tut7().shutdown()
 
 
-def test_redundancy_agent_four_signals():
-    """Test executability of redundancy_agent_four_signals.py."""
+def test_redundancy_agent_four_signals_executability():
     four_signal_redundancy().shutdown()
 
 
-def test_redundancy_agent_one_signal():
-    """Test executability of redundancy_agent_one_signal.py."""
+def test_redundancy_agent_one_signal_executability():
     one_signal_redundancy().shutdown()
 
 
