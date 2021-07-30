1d5e92964b16aa78d80af99a0fdcac57a0cfc779
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-07-30 10:21:23 +0200

    wip(tutorial_1_generator_agent): demonstrate parameter use but simplify tutorial 1

--- agentMET4FOF_tutorials/tutorial_1_generator_agent.py
+++ agentMET4FOF_tutorials/tutorial_1_generator_agent.py
@@ -1,34 +1,6 @@
-from agentMET4FOF.agents import AgentMET4FOF, AgentNetwork, MonitorAgent
-from agentMET4FOF.streams import SineGenerator
+import numpy as np
 
-
-class SineGeneratorAgent(AgentMET4FOF):
-    """An agent streaming a sine signal
-
-    Takes samples from the :py:mod:`SineGenerator` and pushes them sample by sample
-    to connected agents via its output channel.
-    """
-
-    # The datatype of the stream will be SineGenerator.
-    _sine_stream: SineGenerator
-
-    def init_parameters(self):
-        """Initialize the input data
-
-        Initialize the input data stream as an instance of the
-        :py:mod:`SineGenerator` class
-        """
-        self._sine_stream = SineGenerator()
-
-    def agent_loop(self):
-        """Model the agent's behaviour
-
-        On state *Running* the agent will extract sample by sample the input data
-        streams content and push it via invoking :py:method:`AgentMET4FOF.send_output`.
-        """
-        if self.current_state == "Running":
-            sine_data = self._sine_stream.next_sample()  # dictionary
-            self.send_output(sine_data["quantities"])
+from agentMET4FOF.agents import AgentNetwork, MonitorAgent, SineGeneratorAgent
 
 
 def demonstrate_generator_agent_use():
@@ -36,15 +8,27 @@ def demonstrate_generator_agent_use():
     agent_network = AgentNetwork()
 
     # Initialize agents by adding them to the agent network.
-    gen_agent = agent_network.add_agent(agentType=SineGeneratorAgent)
-    monitor_agent = agent_network.add_agent(agentType=MonitorAgent)
+    default_sine_agent = agent_network.add_agent(
+        name="Default Sine generator", agentType=SineGeneratorAgent
+    )
+    custom_sine_agent = agent_network.add_agent(
+        name="Custom Sine generator", agentType=SineGeneratorAgent
+    )
+    custom_sine_agent.init_parameters(
+        sfreq=50,
+        sine_freq=np.pi,
+        amplitude=0.75,
+    )
+    monitor_agent = agent_network.add_agent(
+        name="Showcase a default and a customized sine signal", agentType=MonitorAgent
+    )
 
     # Interconnect agents by either way:
     # 1) by agent network.bind_agents(source, target).
-    agent_network.bind_agents(gen_agent, monitor_agent)
+    agent_network.bind_agents(default_sine_agent, monitor_agent)
 
     # 2) by the agent.bind_output().
-    gen_agent.bind_output(monitor_agent)
+    custom_sine_agent.bind_output(monitor_agent)
 
     # Set all agents' states to "Running".
     agent_network.set_running_state()
