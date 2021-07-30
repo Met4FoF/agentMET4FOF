17a41715175c03eada41036f6b713a2a1deb7f7b
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2021-07-30 18:29:15 +0200

    wip(noise_jitter_removal_agents): repair notebook

--- agentMET4FOF_tutorials/noise_jitter/remove_noise_and_jitter.py
+++ agentMET4FOF_tutorials/noise_jitter/remove_noise_and_jitter.py
@@ -18,7 +18,9 @@ def demonstrate_noise_jitter_removal_agent():
 
     noise_agent = agentNetwork.add_agent(agentType=NoiseAgent)
 
-    njremove_agent = agentNetwork.add_agent(agentType=NoiseJitterRemovalAgent)
+    noise_jitter_removal_agent = agentNetwork.add_agent(
+        agentType=NoiseJitterRemovalAgent
+    )
 
     monitor_agent = agentNetwork.add_agent(
         agentType=MonitorAgent, name="Sine with Noise and Jitter"
@@ -29,11 +31,11 @@ def demonstrate_noise_jitter_removal_agent():
 
     # connect agents : jitter generator -> noise -> njremoval agent
     agentNetwork.bind_agents(sine_with_jitter_agent, noise_agent)
-    agentNetwork.bind_agents(noise_agent, njremove_agent)
+    agentNetwork.bind_agents(noise_agent, noise_jitter_removal_agent)
 
     # connect monitor agents
     agentNetwork.bind_agents(noise_agent, monitor_agent)
-    agentNetwork.bind_agents(njremove_agent, monitor_agent2)
+    agentNetwork.bind_agents(noise_jitter_removal_agent, monitor_agent2)
 
     # set all agents states to "Running"
     agentNetwork.set_running_state()
