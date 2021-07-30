009ed8e8f14447692f1cdbbe7ccaa94b1ac12c53
Anupam Prasad Vedurmudi <anupam.vedurmudi@ptb.de>
2021-07-30 13:36:56 +0200

    wip(noise_jitter_removal_agents): brings module up-to-date and removes duplicated classes

--- agentMET4FOF/agents/noise_jitter_removal_agents.py
+++ agentMET4FOF/agents/noise_jitter_removal_agents.py
@@ -759,37 +759,6 @@ def whrand(istate, N):
 
     return r, istate
 
-
-def njr(fs, ydata, N, niter, tol, m0w, s0w, m0t, s0t, Mc, M0, Nc, Q):
-    analyse_fun = MCMCMH_NJ(fs, ydata, N, niter, tol, m0w, s0w, m0t, s0t, Mc, M0, Nc, Q)
-    yhat1= analyse_fun.AnalyseSignalN()
-    return yhat1
-
-class NJRemoved(AgentMET4FOF):
-    def init_parameters(self, fs=100, ydata = np.array([]),  N=15, niter=100, tol=1e-9, m0w = 10, s0w = 0.0005, m0t = 10, s0t = 0.0002*100/8, Mc=5000, M0=100, Nc=100, Q=50 ):
-        self.fs = fs
-        self.ydata = ydata
-        self.N = N
-        self.niter = niter
-        self.tol = tol
-        self.m0w = m0w
-        self.s0w = s0w
-        self.m0t = m0t
-        self.s0t = s0t
-        self.Mc = Mc
-        self.M0 = M0
-        self.Nc = Nc
-        self.Q = Q
-
-
-    def on_received_message(self, message):
-        ddata = message['data']
-        self.ydata = np.append(self.ydata, ddata)
-        if np.size(self.ydata) == self.N:
-            t = njr(self.fs, self.ydata, self.N, self.niter, self.tol, self.m0w, self.s0w, self.m0t, self.s0t, self.Mc, self.M0,self.Nc, self.Q)
-            self.send_output(self.ydata[7] - t)
-            self.ydata = self.ydata[1:self.N]
-
 def njr(fs, ydata, N, niter, tol, m0w, s0w, m0t, s0t, Mc, M0, Nc, Q):
     analyse_fun = MCMCMH_NJ(fs, ydata, N, niter, tol, m0w, s0w, m0t, s0t, Mc, M0, Nc, Q)
     yhat1= analyse_fun.AnalyseSignalN()
