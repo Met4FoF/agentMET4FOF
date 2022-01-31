f4a991401573c6d5bd8613cbafd3b43e1cd155a6
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2022-01-31 22:11:10 +0100

    build(setup): reintroduce Python 3.8 to 3.9 for repo2docker

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
         "visdcc",
     ],
     extras_require={"tutorials": ["notebook", "PyDynamic"]},
-    python_requires=">=3.10",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
@@ -90,6 +90,8 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
     cmdclass={"verify": VerifyVersionCommand},
